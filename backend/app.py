ced3ea0bf0eece304431df415d20b8b7277aa11b
fatenke <kerroufaten729@gmail.com>
2026-03-31 15:58:42 +0100

    add kubernetes config

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend is running 🚀"})
+
+app.run(host="0.0.0.0", port=5000)
